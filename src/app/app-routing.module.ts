628d876a962f228ce7ceb98a39790e6e1421ce04
alvaromoca1 <tunime.1@gmail.com>
2020-06-19 13:14:57 -0500

    añadiendo las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReporteComponent } from './component/reporte/reporte.component';
+import { CooperativaIndexComponent } from './component/coopertiva/cooperativa-index/cooperativa-index.component';
+import { ProductorIndexComponent } from './component/productor/productor-index/productor-index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'reporte', component: ReporteComponent},
+  {path: 'cooperativo', component: CooperativaIndexComponent},
+  {path: 'productor', component: ProductorIndexComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
